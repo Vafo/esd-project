7e99470bea173ea9887dc64a37edf7774aa3a9cb
vafo <v.safokhujaev@student.inha.uz>
2023-11-14 21:31:53 +0500

    small refactor

--- src/app.c
+++ src/app.c
@@ -19,52 +19,55 @@
 #include "_glcd.h"
 
 
+#include "etc.h"
+
+
 void port_init() {
-  PORTA = 0x00;   DDRA = 0xFF;                    // PA: D
-  PORTB = 0xFF;   DDRB = 0xFF;                    // PB: LED
-  PORTD = 0x00;   DDRD = 0x00;                    // PD: button
-  PORTE = 0x00;   DDRE = 0xFF;                    // PE4: R/S, PE5: E, PE6: CS2, PE7: CS1
-  PORTG = 0x00;   DDRG = 0xFF;                    // PG4: Buzzer
+    PORTA = 0x00;   DDRA = 0xFF;                    // PA: D
+    PORTB = 0xFF;   DDRB = 0xFF;                    // PB: LED
+    PORTD = 0x00;   DDRD = 0x00;                    // PD: button
+    PORTE = 0x00;   DDRE = 0xFF;                    // PE4: R/S, PE5: E, PE6: CS2, PE7: CS1
+    PORTG = 0x00;   DDRG = 0xFF;                    // PG4: Buzzer
 }
 
 game_instance_t game;
 
 int main() {
-  // USB I/O
-  uart1_init();
-  // ADC for Joystick
-  adc_init();
-  // Init ports for LCD
-  port_init();
-  // Init LCD
-  lcd_init();
+    // USB I/O
+    uart1_init();
+    // ADC for Joystick
+    adc_init();
+    // Init ports for LCD
+    port_init();
+    // Init LCD
+    lcd_init();
   
 
-  game_init(&game);
-	sei();								//never forget	
+    game_init(&game);
+    sei();								//never forget	
 
-  _delay_ms(500);
-  while (1)
-  {
-    game_loop_render(&game);
-    game_loop_logic(&game);
-    _delay_ms(100);
-  }
+    _delay_ms(500);
+    while (1)
+    {
+        game_loop_render(&game);
+        game_loop_logic(&game);
+        _delay_ms(100);
+    }
+
   
+    /*ESP*/
+    // uart0_init(ESP_BAUD);
+    // uart1_init(USB_BAUD);
+    // uint8_t buf[128];
+    // size_t size;
+
+    // size_t i;
+    // while(1) {
+    //   size = uart1_recv_arr(buf, 128);
+    //   uart0_send_arr(buf, size);
+    //   size = uart0_recv_arr(buf, 128);
+    //   uart1_send_arr(buf, size);
+    // }
   
-  /*ESP*/
-  // uart0_init(ESP_BAUD);
-  // uart1_init(USB_BAUD);
-  // uint8_t buf[128];
-  // size_t size;
-  
-  // size_t i;
-  // while(1) {
-  //   size = uart1_recv_arr(buf, 128);
-  //   uart0_send_arr(buf, size);
-  //   size = uart0_recv_arr(buf, 128);
-  //   uart1_send_arr(buf, size);
-  // }
-  
-  return 0;
+    return 0;
 }
